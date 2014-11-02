bcbddc7dcbb1d4efc56a799f7b057fbb4baf8e47
wawbmd <wawbdc@gmail.com>
2014-11-01 23:26:48 -0400

    Adding freqTestCode

---
+++ freqTest/SineWave.h
@@ -0,0 +1,42 @@
+#ifndef __sine_wave_h_
+#define __sine_wave_h_
+
+#include "AL/al.h"
+#include "AL/alc.h"
+
+class SineWave 
+{
+public:
+	SineWave(ALCdevice *openedDevice, ALint sampleRate = 44100);
+    ~SineWave();
+
+    typedef enum _tag_thingy_
+    {
+        MONO,
+        STEREO,
+        STEREO_LEFT,
+        STEREO_RIGHT
+    } Channel;
+
+    void CreateWave( Channel outputChannel, ALint startFreq, ALint endFreq, ALint step, ALint seconds );
+    void Play();
+
+private:
+
+	SineWave();
+	SineWave(SineWave&);
+    void Cleanup();
+
+    ALuint m_buffer;
+    ALuint m_source;
+    Channel m_outputFormat;
+    
+    ALint m_sampleRate;
+    ALushort *m_audioData;
+    ALuint m_frames;
+    ALCdevice *m_device;
+	ALCcontext *m_context;
+};
+
+
+#endif
---
+++ freqTest/source2.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "AL/al.h"
+#include "AL/alc.h"
+
+
